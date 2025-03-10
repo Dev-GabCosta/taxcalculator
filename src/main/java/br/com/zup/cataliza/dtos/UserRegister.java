2baccd2b4295e6cd1c7ee36c89b4060ffd7cedc7
Gabriel Costa <gabriel.costa@zup.com.br>
2025-03-10 11:55:52 -0300

    feat(UserServiceTest): adicionar teste para o serviço de criação dde um usuário

---
+++ src/main/java/br/com/zup/cataliza/dtos/UserRegister.java
@@ -0,0 +1,12 @@
+package br.com.zup.cataliza.dtos;
+
+import br.com.zup.cataliza.models.Roles;
+
+import java.util.List;
+
+public record UserRegister(
+		String username,
+		String password,
+		List<Roles> roles
+) {
+}
---
+++ src/test/java/br/com/zup/cataliza/services/UserServiceTest.java
@@ -0,0 +1,43 @@
+package br.com.zup.cataliza.services;
+
+import br.com.zup.cataliza.dtos.UserRegister;
+import br.com.zup.cataliza.dtos.UserResponse;
+import br.com.zup.cataliza.models.User;
+import br.com.zup.cataliza.repositories.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+
+	@Test
+	void testCreateUser() throws NoSuchFieldException, IllegalAccessException{
+		String username = "gabs3452";
+		String password = "senha3452";
+		UserRepository userRepository = mock(UserRepository.class);
+		UserService userService= new UserService(userRepository);
+		UserRegister userRegister = new UserRegister(username, password, null);
+		when(userRepository.save(any(User.class))).thenAnswer(
+				invocation -> {
+					User user = invocation.getArgument(0);
+					Field idField = User.class.getDeclaredField("id");
+					idField.setAccessible(true);
+					idField.set(user, 1L);
+					return user;
+				}
+		);
+		UserResponse createdUser = userService.createUser(userRegister);
+		assertNotNull(createdUser);
+		assertEquals(1L, createdUser.id());
+		assertEquals(username, createdUser.username());
+		assertEquals(password, createdUser.password());
+		verify(userRepository, times(1)).save(any(User.class));
+	}
+
+
+}
