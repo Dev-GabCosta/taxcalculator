84a9c4c9cb4c990c07b81ac51c92fa7aea20ab88
Gabriel Costa <gabriel.costa@zup.com.br>
2025-03-11 11:40:08 -0300

    refactor(UserService): Adicionar interface UserService
    Cria interface UserService para desacoplar o controlador do serviço de usuários

--- src/main/java/br/com/zup/cataliza/services/UserService.java
+++ src/main/java/br/com/zup/cataliza/services/UserService.java
@@ -1,28 +1,8 @@
 package br.com.zup.cataliza.services;
 
-import br.com.zup.cataliza.repositories.UserRepository;
 import br.com.zup.cataliza.dtos.UserRegister;
 import br.com.zup.cataliza.dtos.UserResponse;
-import br.com.zup.cataliza.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
-private final UserRepository userRepository;
-
-	public UserService(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-
-	public UserResponse createUser(UserRegister userRegister){
-		User user = new User(userRegister.username(), userRegister.password());
-		userRepository.save(user);
-		return new UserResponse(
-				user.getId(),
-				user.getUsername(),
-				user.getPassword()
-		);
-	}
-
+public interface UserService {
+	UserResponse createUser(UserRegister userRegister);
 }
