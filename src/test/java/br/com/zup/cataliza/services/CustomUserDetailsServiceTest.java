71f4b205ab559df4e728525c918d7ba55d17608c
Gabriel Costa <gabriel.costa@zup.com.br>
2025-03-20 13:17:36 -0300

    feat(CustomUserDetailsService, CustomUserDetailsServiceTest): Adicionar classes UserDetailsService, UserDetailsServiceTest
    - Implementa a interface UserDetails por meio da classe UserDetailsService.
    - Implementa teste para a classe UserDetailsService

---
+++ src/test/java/br/com/zup/cataliza/services/CustomUserDetailsServiceTest.java
@@ -0,0 +1,49 @@
+package br.com.zup.cataliza.services;
+
+import br.com.zup.cataliza.models.User;
+import br.com.zup.cataliza.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CustomUserDetailsServiceTest {
+	private UserRepository userRepository = Mockito.mock(UserRepository.class);
+
+	@InjectMocks
+	private CustomUserDetailsService customUserDetailsService;
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testLoadUserByUsername() {
+		String username = "mariofernandes3852";
+		String password = "mariopassword";
+		User mockUser = new User(username, password);
+		mockUser.setRoles(new HashSet<>());
+
+		Mockito.when(userRepository.findByUsername(username))
+				.thenReturn(Optional.of(mockUser));
+
+		UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+		assertNotNull(userDetails);
+		assertEquals(username, userDetails.getUsername());
+		assertEquals(password, userDetails.getPassword());
+
+		Mockito.verify(userRepository, Mockito.times(1))
+				.findByUsername(username);
+	}
+
+}
