d523901f0748ca84c81f89c7808569849485c8f3
Dev-GabCosta <gabriel.costa@zup.com.br>
2025-04-10 16:33:36 -0300

    Modificar testes

--- src/test/java/br/com/zup/cataliza/TaxcalculatorApplicationTests.java
+++ src/test/java/br/com/zup/cataliza/TaxcalculatorApplicationTests.java
@@ -3,9 +3,11 @@ package br.com.zup.cataliza;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class TaxcalculatorApplicationTests {
 
 	@Test
